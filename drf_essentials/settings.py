5f5c905f734b6173a405582fae320e589a925c8f
johnrearden <isnocturnal@gmail.com>
2023-11-15 15:57:28 +0000

    Add post app and image validation

--- drf_essentials/settings.py
+++ drf_essentials/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'cloudinary',
     'rest_framework',
     'profiles',
+    'posts',
 ]
 
 MIDDLEWARE = [
--- profiles/serializers.py
+++ profiles/serializers.py
@@ -10,9 +10,25 @@ class ProfileSerializer(serializers.ModelSerializer):
         request = self.context['request']
         return request.user == obj.owner
 
+    def validate_image(self, value):
+        if value.size > 1024 * 1024 * 2:
+            raise serializers.ValidationError(
+                'Image size is larger than 2MB!'
+            )
+        if value.image.width > 4096:
+            raise serializers.ValidationError(
+                'Image width > 4096px'
+            )
+        if value.image.height > 4096:
+            raise serializers.ValidationError(
+                'Image height > 4096px'
+            )
+        return value
+
+
     class Meta:
         model = Profile
         fields = [
             'id', 'owner', 'created_at', 'updated_at', 'name', 
-            'content', 'image', 'is_owner',
+            'content', 'image', 'is_owner', 'image_filter',
         ]
