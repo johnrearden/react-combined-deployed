dbf9b0b886e6397d0198eae556c54834a54767f4
johnrearden <isnocturnal@gmail.com>
2023-11-22 20:49:46 +0000

    Add whitenoise to serve admin static files

--- drf_essentials/settings.py
+++ drf_essentials/settings.py
@@ -75,6 +75,7 @@ SITE_ID = 1
 
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
